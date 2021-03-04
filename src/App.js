3babf5dc4e364e8fc7866dcadbc8c717ec1d2a8f
shrestha harry <hsoftwarica50@gmail.com>
2021-03-05 00:00:05 +0545

    netflix clone project-first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// 5f110dbb570520136fb124bb54e83936
+
+// https://api.themoviedb.org/3/movie/550?api_key=5f110dbb570520136fb124bb54e83936 -----for testing in postman 
+
+export default App;
