dbe0cd5b6ed289e9710a5ab3cdea03c08d1e5ca9
shrestha harry <hsoftwarica50@gmail.com>
2021-03-08 17:27:04 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Row from './Row'
+import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+			<Banner />
+			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
@@ -27,3 +25,5 @@ function App() {
 // https://api.themoviedb.org/3/movie/550?api_key=5f110dbb570520136fb124bb54e83936 -----for testing in postman 
 
 export default App;
+
+
